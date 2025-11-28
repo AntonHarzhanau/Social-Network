2d765ee04e18b5e6f1a8654ac0fbf0684b72b446
Anton Harzhanau <anton.harzhanau@gmail.com>
2025-11-28 14:34:47 +0100

    init shadcn

---
+++ client/src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/shared/components/ui/button'
+
+function App() {
+
+  return (
+    <div className='flex justify-center min-h-screen items-center'>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
